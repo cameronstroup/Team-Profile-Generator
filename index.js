60516059df9eece03a2f96017cbc5169b73445ef
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-04-18 11:48:40 -0500

    finished questions

--- index.js
+++ index.js
@@ -1,37 +1,103 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const path = require("path");
+const fs = require("fs");
 
-const employeeInfo = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "what is your ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your prefered email address?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is your github address?",
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "What is your role in the company?",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "What is your prefered email address?",
-    },
-  ]);
-};
+let employeeQuestionArray = [];
+const whatEmployee = [
+  {
+    type: "list",
+    name: "type",
+    message: "What is thier role in the company?",
+    choices: ["Intern", "Manager", "Engineer"],
+  },
+];
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    messsage: "What is the name of the Manager?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their ID Number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    mesage: "What is their email?",
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "What is their office number?",
+  },
+  {
+    type: "list",
+    name: "last",
+    message: "Have you input all employees?",
+    choices: ["Yes", "No"],
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    messsage: "What is the name of the engineer?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    mesage: "What is their email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is their github username?",
+  },
+  {
+    type: "list",
+    name: "InputAll",
+    message: "Have you input all employees?",
+    choices: ["Yes", "No"],
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    messsage: "What is the name of the intern?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is thier ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    mesage: "What is thier email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Whitch school does this intern go to?",
+  },
+  {
+    type: "list",
+    name: "last",
+    message: "Have you input all employees?",
+    choices: ["Yes", "No"],
+  },
+];
